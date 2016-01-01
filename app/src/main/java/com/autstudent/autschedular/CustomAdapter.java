f54d6d68517edf4ea8175c16326edc9de0a01560
wilzonejoe <wilzonejoe@gmail.com>
2016-01-01 17:48:19 +1300

    Revert "LocalDataStorage"

--- app/src/main/java/com/autstudent/autschedular/CustomAdapter.java
+++ app/src/main/java/com/autstudent/autschedular/CustomAdapter.java
@@ -1,30 +1,26 @@
 package com.autstudent.autschedular;
 
 import android.app.Activity;
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-import com.parse.ParseObject;
-
-import java.util.Calendar;
 import java.util.List;
 
 /**
  * Created by wilzo on 26/12/2015.
  */
 public class CustomAdapter extends ArrayAdapter {
-    private Context activity;
-    private List<ParseObject> objects;
+    private Activity activity;
+    private List<String> title;
 
 
-    public CustomAdapter(Context context, int resource, List<ParseObject> objects) {
-        super(context, resource, objects);
+    public CustomAdapter(Activity context, int resource, List<String> Title) {
+        super(context, resource, Title);
         this.activity = context;
-        this.objects= objects;
+        this.title = Title;
     }
 
     @Override
@@ -33,7 +29,7 @@ public class CustomAdapter extends ArrayAdapter {
         ViewHolder view;
         if (rowView == null) {
             // Get a new instance of the row layout view
-            LayoutInflater inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            LayoutInflater inflater = activity.getLayoutInflater();
             rowView = inflater.inflate(R.layout.custom_row_layout, null);
             // Hold the view objects in an object, that way the don't need to be "re- finded"
             view = new ViewHolder();
@@ -45,34 +41,8 @@ public class CustomAdapter extends ArrayAdapter {
         }
         /** Set data to your Views. */
 
-        String day = "Sunday";
-        int dayCode = Integer.parseInt(objects.get(position).get("Day").toString());
-        switch (dayCode){
-            case Calendar.MONDAY:
-                day = "Monday";
-                break;
-            case Calendar.TUESDAY:
-                day = "Tuesday";
-                break;
-            case Calendar.WEDNESDAY:
-                day = "Wednesday";
-                break;
-            case Calendar.THURSDAY:
-                day = "Thursday";
-                break;
-            case Calendar.FRIDAY:
-                day = "Friday";
-                break;
-            case Calendar.SATURDAY:
-                day = "Saturday";
-                break;
-            case Calendar.SUNDAY:
-                day = "Sunday";
-                break;
-        }
-
-        view.title.setText(day);
-        view.details.setText(objects.get(position).get("Activity").toString());
+        view.title.setText(title.get(position));
+        view.details.setText(title.get(position));
 
         return rowView;
     }
