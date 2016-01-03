3a08143f09ccb53744b713623f9140320979cbcd
wilzonejoe <wilzonejoe@gmail.com>
2016-01-03 23:12:09 +1300

    aa

--- app/src/main/java/com/autstudent/autschedular/DetailsActivity.java
+++ app/src/main/java/com/autstudent/autschedular/DetailsActivity.java
@@ -1,10 +1,20 @@
 package com.autstudent.autschedular;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class DetailsActivity extends AppCompatActivity {
+
+    private static final int VIEW = 1;
+    private static final int EDIT = 2;
+
+    private boolean editMode = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,14 +24,37 @@ public class DetailsActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        final FloatingActionButton fab1 = (FloatingActionButton) findViewById(R.id.fab1);
+
+        fab1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!editMode) {
+                    editMode = true;
+                    changeLayout(DetailsActivity.EDIT);
+                }
+                else {
+                    editMode = false;
+                    changeLayout(DetailsActivity.VIEW);
+                }
+            }
+        });
+        changeLayout(DetailsActivity.VIEW);
+    }
+
+    private void changeLayout(int status) {
+        Fragment fragment = new DetailViewFrag();
+        switch (status) {
+            case DetailsActivity.VIEW:
+                fragment = new DetailViewFrag();
+                break;
+            case DetailsActivity.EDIT:
+                fragment = new DetailEditFrag();
+                break;
+        }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.detail_fragment, fragment).commit();
     }
 
 }
